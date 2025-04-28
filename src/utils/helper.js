c18a84e9e8c4188ea92d73318074a0872e0c8267
khushboo-590 <khushbook4979@gmail.com>
2025-04-28 16:14:05 +0530

    add img

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,32 +1,32 @@
-import Card1 from '../assets/images/png/Card-1.png';
-import Card2 from '../assets/images/png/Card-2.png';
-import Card3 from '../assets/images/png/Card-3.png';
-import Card4 from '../assets/images/png/Card-4.png';
+import card1 from '../assets/images/png/card-1.png';
+import card2 from '../assets/images/png/card-2.png';
+import card3 from '../assets/images/png/card-3.png';
+import card4 from '../assets/images/png/card-4.png';
 
 export const cardLIST = [
     {
-        img: Card1,
+        img: card1,
         alt: 'step-location',
         imgContent: '1. Enter Your Location and Preferences',
         title: '1. Your Location',
         description: 'Enter your location and preferences to find the best energy, gas, and internet plans for you.'
     },
     {
-        img: Card2,
+        img: card2,
         alt: 'step-compare',
         imgContent: '2. Compare Providers',
         title: '2. Compare',
         description: 'Lorem ipsum dolor sit amet consectetur. Semper vitae nullam eget consectetur mi. Vulputate sapien a a bibendum'
     },
     {
-        img: Card3,
+        img: card3,
         alt: 'step-switch-easily',
         imgContent: '3. Switch Easily',
         title: '3. Switch Easily',
         description: 'Complete the process in just a few clicks. We’ll handle the rest and ensure a smooth transition to your new provider.'
     },
     {
-        img: Card4,
+        img: card4,
         alt: 'step-best-plan',
         imgContent: '4. Choose the Best Plan',
         title: '4. Best Plan',
