6a7daa44d3b15ab860dbb5f9ca38ef7bf11cf7b2
khushboo-590 <khushbook4979@gmail.com>
2025-04-28 15:54:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+import Slider from './components/Slider'; 
+
+function App() {
+  return (
+    <>
+      <Slider /> 
+    </>
+  );
+}
+
+export default App;
+
---
+++ src/utils/helper.js
@@ -0,0 +1,35 @@
+import Card1 from '../assets/images/png/Card-1.png';
+import Card2 from '../assets/images/png/Card-2.png';
+import Card3 from '../assets/images/png/Card-3.png';
+import Card4 from '../assets/images/png/Card-4.png';
+
+export const cardLIST = [
+    {
+        img: Card1,
+        alt: 'step-location',
+        imgContent: '1. Enter Your Location and Preferences',
+        title: '1. Your Location',
+        description: 'Enter your location and preferences to find the best energy, gas, and internet plans for you.'
+    },
+    {
+        img: Card2,
+        alt: 'step-compare',
+        imgContent: '2. Compare Providers',
+        title: '2. Compare',
+        description: 'Lorem ipsum dolor sit amet consectetur. Semper vitae nullam eget consectetur mi. Vulputate sapien a a bibendum'
+    },
+    {
+        img: Card3,
+        alt: 'step-switch-easily',
+        imgContent: '3. Switch Easily',
+        title: '3. Switch Easily',
+        description: 'Complete the process in just a few clicks. We’ll handle the rest and ensure a smooth transition to your new provider.'
+    },
+    {
+        img: Card4,
+        alt: 'step-best-plan',
+        imgContent: '4. Choose the Best Plan',
+        title: '4. Best Plan',
+        description: 'Enter your location and preferences to find the best energy, gas, and internet plans for you.'
+    },
+];
